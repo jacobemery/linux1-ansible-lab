f90a93f3723502d0ef85f74f90693ed09131bcbc
Jacob Emery <jacob.emery@ibm.com>
2023-02-13 19:14:07 -0800

    remove ./ from paths
    Signed-off-by: Jacob Emery <jacob.emery@ibm.com>

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 sudo dnf install ansible -y
-ansible-galaxy collection install -r ./files/requirements.yaml
-ansible-playbook ./playbooks/setup.yaml
+ansible-galaxy collection install -r files/requirements.yaml
+ansible-playbook playbooks/setup.yaml
